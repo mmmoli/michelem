82a70cbd9802127512d56dab3ceedd8d9b6d3309
Michele Memoli <michele.memoli@gmail.com>
2023-09-18 17:07:09 +0100

    style(tailwind): added config
    Default tailwind configuration added.

--- apps/web/app/_components/main-message/main-message.tsx
+++ apps/web/app/_components/main-message/main-message.tsx
@@ -1,3 +1,9 @@
 export const MainMessage = () => {
-  return <h1 data-testid="main-message">Michele's Main Message</h1>;
+  return (
+    <section data-testid="main-message">
+      <h1 className="text-3xl bg-slate-400">We need better tools.</h1>
+      <p>Better software, interfaces, interactions.</p>
+      <p>Better conversations, goals and prospects.</p>
+    </section>
+  );
 };
